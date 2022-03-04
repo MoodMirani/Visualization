21e3fc73161e9d0ce4d97c67d14bcba2d58a6a90
Ladydiana <diana.culincu@gmail.com>
2022-03-04 09:51:58 +0100

    Triangle with same side

--- Lab06/triangle.cpp
+++ Lab06/triangle.cpp
@@ -26,6 +26,9 @@ Triangle::Triangle(const vec3& v0, const vec3& v1, const vec3& v2, const vec3& u
     mUVW[2] = uvw2;
 }
 
+
+
+
 bool Triangle::closestIntersection(const Ray& ray, double maxLambda,
                                    RayIntersection& intersection) const {
     // Programming TASK 1: Implement this method
@@ -43,8 +46,11 @@ bool Triangle::closestIntersection(const Ray& ray, double maxLambda,
     // Ray direction t_r : ray.getDirection()
     // Compute the intersection point using ray.pointOnRay(lambda)
 
+    
+
     vec3 tr = ray.getDirection();
     vec3 pr = ray.getOrigin();
+    vec3 uvw(0, 0, 0);
 
     vec3 p0 = mVertices[0];
     vec3 p1 = mVertices[1];
@@ -56,43 +62,40 @@ bool Triangle::closestIntersection(const Ray& ray, double maxLambda,
     vec3 t2 = p2 - p0;
 
     // n=t1xt2
-    vec3 normal = glm::normalize(glm::cross(t1, t2));
+    vec3 n = glm::normalize(glm::cross(t1, t2));
 
-   double lambda = 0;
+    double lambda = -1;
 
     // lambda = (p0-pr) x n/(tr x n)
     // page 17. In practice tr x n > epsilon
-    if (fabs(dot(tr, normal)) > Util::epsilon) {
-        lambda = dot(p0 - pr, normal) / dot(tr, normal);
+    if (fabs(dot(tr, n)) > Util::epsilon) {
+        lambda = dot(p0 - pr, n) / dot(tr, n);
     } 
     else
         return false;
 
-
-    //why?
-    if (lambda < 0.0 || lambda + Util::epsilon > maxLambda) {
+    if (lambda < 0 || lambda > maxLambda) {
         return false;
     }
 
-    vec3 p = ray.pointOnRay(lambda) - p0;  // point on ray
+    vec3 p = ray.pointOnRay(lambda);
 
-    // barycentric coordinates ?
+    //check if point inside triangle geometrically
+    //https://ppt-online.org/703109 slide 25 (University of Virginia 445)
+    vec3 cp1 = cross(p2 - p1, p - p1);
+    vec3 cp2 = cross(p2 - p1, p0 - p1);
+    vec3 cp3 = cross(p2 - p0, p - p0);
+    vec3 cp4 = cross(p2 - p0, p1 - p0);
+    vec3 cp5 = cross(p1 - p0, p - p0);
+    vec3 cp6 = cross(p1 - p0, p2 - p0);
+    if (dot(cp1, cp2) < 0) return false;
+    if (dot(cp3, cp4) < 0) return false;
+    if (dot(cp5, cp6) < 0) return false;
 
-    float denomenator2 = (dot(t1, t2) * dot(t1, t2) - dot(t1, t1) * dot(t2, t2));
+        
+    intersection = RayIntersection(ray, shared_from_this(), lambda, ray.pointOnRay(lambda), uvw);
+    return true;
 
-    // ??
-    float lambda1 = (dot(t1, t2) * dot(p, t2) - dot(t2, t2) * dot(p, t1)) / denomenator2;
-    float lambda2 = (dot(t1, t2) * dot(p, t2) - dot(t1, t1) * dot(p, t2)) / denomenator2;
-
-    // lambda 1 and 2 must be 0 or higher, sum cannot be above 1
-    // also need to add checks if lambda1 and lambda2 less than 1, page 18
-    if (lambda1 >= 0.0 && lambda2 >= 0.0 && (lambda1 + lambda2 <= 1.0)) {
-        vec3 uvw(0, 0, 0);
-        intersection = RayIntersection(ray, shared_from_this(), lambda, normal, uvw);
-        return true;
-    }
-
-    return false;
 }
 
 bool Triangle::anyIntersection(const Ray& ray, double maxLambda) const {
